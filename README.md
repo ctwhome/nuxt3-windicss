f2ae6be8578b8a37cd986a1f0319af0cc15c8708
ctwhome <ctw@ctwhome.com>
2021-10-29 11:53:26 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# nuxt3-windicss-jhdrfx
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/nuxt3-windicss-jhdrfx)
---
+++ components/Compo.vue
@@ -0,0 +1,5 @@
+<template>
+  mu c ompo not it chanes no problem this is it 
+  <div class="bg-blue-500 m-12">adsfasdf I can't believe how fast it is this is it!</div> 
+
+</template>
